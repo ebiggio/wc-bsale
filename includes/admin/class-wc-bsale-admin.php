efab6da1f19ffac05eb6f08ca1a577b5ed1515df
Enzo Biggio <ebiggio@gmail.com>
2024-03-07 22:05:09 -0300

    Removed redundant plugin status validation, added user permissions validation and doc blocks

--- includes/admin/class-wc-bsale-admin.php
+++ includes/admin/class-wc-bsale-admin.php
@@ -1,31 +1,47 @@
 <?php
+/**
+ * WC Bsale Admin
+ *
+ * Entry point for the plugin's admin functionality. It enqueues the admin styles and adds the settings page to the admin menu.
+ *
+ * @package WC_Bsale
+ */
 
 namespace WC_Bsale;
 
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * WC Bsale Admin class
+ */
 class WC_Bsale_Admin {
 	private $settings;
 
 	public function __construct() {
-		// If the plugin is active, load the admin settings and the hooks
-		if ( in_array( 'wc-bsale/wc-bsale.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-			// Plugin's admin styles
-			add_action( 'admin_enqueue_scripts', function () {
-				wp_enqueue_style( 'wc-bsale-admin', WC_BSALE_PLUGIN_URL . 'assets/css/wc-bsale.css', array(), WC_BSALE_PLUGIN_VERSION );
-			} );
+		// Plugin's admin styles
+		add_action( 'admin_enqueue_scripts', function () {
+			wp_enqueue_style( 'wc-bsale-admin', WC_BSALE_PLUGIN_URL . 'assets/css/wc-bsale.css', array(), WC_BSALE_PLUGIN_VERSION );
+		} );
 
-			require_once plugin_dir_path( __FILE__ ) . 'class-wc-bsale-admin-settings.php';
+		require_once plugin_dir_path( __FILE__ ) . 'class-wc-bsale-admin-settings.php';
 
-			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
-			$this->settings = new WC_Bsale_Admin_Settings();
+		add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+		$this->settings = new WC_Bsale_Admin_Settings();
 
-			require_once plugin_dir_path( __FILE__ ) . 'class-wc-bsale-admin-hooks.php';
-			new WC_Bsale_Admin_Hooks();
-		}
+		require_once plugin_dir_path( __FILE__ ) . 'class-wc-bsale-admin-hooks.php';
 	}
 
-	public function add_admin_menu() {
+	/**
+	 * Add the settings page to the admin menu in the back office of WordPress
+	 *
+	 * @return void
+	 */
+	public function add_admin_menu(): void {
+		// Check if the user has the necessary permissions to access the settings
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
 		add_menu_page(
 			'WooCommerce Bsale plugin settings', // Page title
 			'WooCommerce Bsale', // Menu title
