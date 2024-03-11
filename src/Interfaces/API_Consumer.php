1f47b06a6fce8ec55b07f9350c143c23bae99dde
Enzo Biggio <ebiggio@gmail.com>
2024-03-11 19:34:26 -0300

    Interfaces for logging and API usage

---
+++ src/Interfaces/API_Consumer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Interface for the classes that will consume the Bsale API.
+ *
+ * @class   API_Consumer
+ * @package WC_Bsale
+ */
+
+namespace WC_Bsale\Interfaces;
+
+interface API_Consumer {
+	/**
+	 * Adds an observer to the list of observers.
+	 *
+	 * @param \WC_Bsale\Interfaces\Observer $observer
+	 *
+	 * @return void
+	 */
+	public function add_observer( Observer $observer ): void;
+
+	/**
+	 * Notifies all the observers of an event.
+	 *
+	 * @param string $event_trigger The event that triggered the operation.
+	 * @param string $event_type    The type of event (for example, stock update, invoice generation, etc.).
+	 * @param string $identifier    An identifier for the event (for example, the product ID, the order ID, etc.).
+	 * @param string $message       The message to log, with details about the operation.
+	 * @param string $result_code   Result code for the operation. Can only be 'info', 'success', 'warning' or 'error'.
+	 *
+	 * @return void
+	 */
+	public function notify_observers( string $event_trigger, string $event_type, string $identifier, string $message, string $result_code = 'info' ): void;
+}
---
+++ src/Interfaces/Observer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Interface for the observer pattern.
+ *
+ * @class   Observer
+ * @package WC_Bsale
+ */
+
+namespace WC_Bsale\Interfaces;
+
+interface Observer {
+	/**
+	 * Updates the observer with details about an operation.
+	 *
+	 * @param string $event_trigger The event that triggered the operation.
+	 * @param string $event_type    The type of event (for example, stock update, invoice generation, etc.).
+	 * @param string $identifier    An identifier for the event (for example, the product ID, the order ID, etc.).
+	 * @param string $message       The message to log, with details about the operation.
+	 * @param string $result_code   Result code for the operation. Can only be 'info', 'success', 'warning' or 'error'.
+	 *
+	 * @return void
+	 */
+	public function update( string $event_trigger, string $event_type, string $identifier, string $message, string $result_code = 'info' ): void;
+}
